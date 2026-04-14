8a8c73ecd9095da3c38d2b5533fd30ee5b3a4f53
Morten Kjelsrud <mortkjels@gmail.com>
2026-04-14 13:35:57 +0200

    Thought process

--- project/src/analysis_log.py
+++ project/src/analysis_log.py
@@ -9,3 +9,8 @@ file_path = current_path.parent / "dataset" / "synthetic_dataset_log.csv"
 df = pd.read_csv(file_path)
 
 print(df.head())
+
+
+#Thinking about Decision-Tree prediction rather than Log.Reg, since
+#There is not always yes, or always no in network traffic due to nuances.
+#Therefore deciding on decision tree.
