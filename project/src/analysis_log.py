023d8cd1b007868d1d5bd4afeb928605fe248888
Morten Kjelsrud <mortkjels@gmail.com>
2026-04-20 17:25:58 +0200

    Fixed the environment issues, starting now

--- project/src/analysis_log.py
+++ project/src/analysis_log.py
@@ -3,24 +3,21 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
+print("Laster inn datasettet")
 df = pd.read_csv("./project/dataset/synthetic_dataset_log.csv")
 
 print(df.head())
 
-# Have not done EDA, just process in the start for correct syntax. 
-
-X = df.drop('Label', axis=1)
+X = df.drop('Label', axis=1).select_dtypes(include=['number'])
 y = df['Label']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-DTC = DecisionTreeClassifier(criterion='log_loss', max_depth=5, random_state=42)
+DTC = DecisionTreeClassifier(criterion='gini', max_depth=5, random_state=42)
 
 d = DTC.fit(X_train,y_train)
 
 
-#Thinking about Decision-Tree prediction rather than Log.Reg, since
-#There is not always yes, or always no in network traffic due to nuances.
-#Therefore deciding on decision tree.
 
